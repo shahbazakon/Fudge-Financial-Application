5ed738beb5e8aa833bdadec44d29fe7788ef7c43
shahbazakon <shahbajakon@gmail.com>
2022-10-28 23:25:28 +0530

    colors and assets added

---
+++ lib/contants/assets_path.dart
@@ -0,0 +1,11 @@
+class AssetsPath {
+  static const String user = "assets/png/user.png";
+  static const String graph = "assets/png/graph.png";
+  static const String bell = "assets/png/bell.png";
+  static const String checkCircle = "assets/png/check_circle.png";
+  static const String processCircle = "assets/png/process_circle.png";
+  static const String dp1 = "assets/png/dp1.png";
+  static const String dp2 = "assets/png/dp2.png";
+  static const String dp3 = "assets/png/dp3.png";
+  static const String dp4 = "assets/png/dp4.png";
+}
